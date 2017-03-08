8a829293dd6704e902ddf319b66ad2a91991a6e5
LaylaAddi <s.chesnowitz@gmail.com>
2017-03-08 07:16:28 -0500

    navigate to users

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { UsersPage } from "../users/users";
 
 @Component({
   selector: 'page-home',
@@ -11,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
     
   }
-
+  onGoToUsers() {
+    this.navCtrl.push(UsersPage);
+  }
 }
