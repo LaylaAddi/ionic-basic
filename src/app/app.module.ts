a00a89225ff9f0fa824cd11177f58e68a444f822
LaylaAddi <s.chesnowitz@gmail.com>
2017-03-08 07:54:50 -0500

    passing user name into page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,24 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { UsersPage } from "../pages/users/users"; 
+import { UsersPage } from "../pages/users/users";
+import { UserPage } from "../pages/users/user/user";
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    UsersPage
+    UsersPage,
+    UserPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp) 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    UsersPage
+    UsersPage,
+    UserPage
   ], 
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
